9029a8fde9e71c69e158e4a126496d2244ae2c44
Alex <82488910+alexgorshkov02@users.noreply.github.com>
2021-08-16 23:55:43 -0400

    Code refactoring

--- routes/api/pizza-routes.js
+++ routes/api/pizza-routes.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const {
-    getAllPizza,
-    getPizzaById,
-    createPizza,
-    updatePizza,
-    deletePizza
-  } = require('../../controllers/pizza-controller');
+  getAllPizza,
+  getPizzaById,
+  createPizza,
+  updatePizza,
+  deletePizza
+} = require('../../controllers/pizza-controller');
 
 // /api/pizzas
 router
@@ -19,5 +19,5 @@ router
   .get(getPizzaById)
   .put(updatePizza)
   .delete(deletePizza);
-  
+
 module.exports = router;
